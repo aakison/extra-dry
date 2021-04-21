8cce091ccc46cb6efafe0d30889b9a64d06005ec
Adrian <adrian@akison.com>
2021-04-21 21:34:25 +1000

    Add shared class for samples, with code analysis

---
+++ Blazor.ExtraDry/Sample.Shared/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.ExtraDry.Sample.Shared
+{
+    public class Employee
+    {
+        [Rules(UpdateAction.BlockChanges)]
+        [Key]
+        public int Id { get; set; }
+
+        public Guid UniqueId { get; set; }
+
+        [Required, MaxLength(50)]
+        [Rules(UpdateAction.AllowChanges)]
+        [Display(Name = "First Name", ShortName = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        [Rules(UpdateAction.AllowChanges)]
+        [Display(Name = "Last Name", ShortName = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blazor-extra-dry
 Performant blazor client library extending DRY principles.
+
