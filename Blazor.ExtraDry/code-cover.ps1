e3c7474839ee27677a8638f42021897dcb0b2ebf
Adrian Akison <adrian@akison.com>
2021-08-30 17:18:25 +1000

    Update code coverage helper script and confirm all tests passing and 100% code coverage on Update methods

--- Blazor.ExtraDry/code-cover.ps1
+++ Blazor.ExtraDry/code-cover.ps1
@@ -1,3 +1,6 @@
+rmdir ./TestCoverage -Recurse
+rmdir ./Blazor.ExtraDry.Tests/TestResults -Recurse
+rmdir ./Blazor.ExtraDry.Core.Tests/TestResults -Recurse
 dotnet test --collect:"XPlat Code Coverage"
 reportgenerator -reports:./*/TestResults/*/*.xml -targetdir:./TestCoverage
 ./TestCoverage/index.htm
