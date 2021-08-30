1908b20b3bf6997a7d14692824e5a7926f6f8875
Adrian Akison <adrian@akison.com>
2021-08-30 13:27:29 +1000

    Improve custom serializer with Json Options

--- Blazor.ExtraDry/Sample.Shared/Convertors/SectorInfoJsonConverter.cs
+++ Blazor.ExtraDry/Sample.Shared/Convertors/SectorInfoJsonConverter.cs
@@ -5,6 +5,10 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Sample.Shared.Converters {
+
+    /// <summary>
+    /// Converts a subset of a `Sector` for use when `Sector` is a child of another object.
+    /// </summary>
     public class SectorInfoJsonConverter : JsonConverter<Sector> {
         public override Sector Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
@@ -12,19 +16,18 @@ namespace Sample.Shared.Converters {
                 throw new JsonException();
             }
             var sector = new Sector();
+            var comparison = options.PropertyNameCaseInsensitive ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
             while(reader.Read()) {
                if(reader.TokenType == JsonTokenType.PropertyName) {
                     var property = reader.GetString();
                     reader.Read();
                     if(reader.TokenType == JsonTokenType.String) {
                         var value = reader.GetString();
-                        switch(property) {
-                            case nameof(Sector.UniqueId):
-                                sector.UniqueId = Guid.Parse(value);
-                                break;
-                            case nameof(Sector.Title):
-                                sector.Title = value;
-                                break;
+                        if(nameof(Sector.UniqueId).Equals(property, comparison)) {
+                            sector.UniqueId = Guid.Parse(value);
+                        }
+                        else if(nameof(Sector.Title).Equals(property, comparison)) {
+                            sector.Title = value;
                         }
                     }
                     else {
@@ -41,9 +44,12 @@ namespace Sample.Shared.Converters {
         public override void Write(Utf8JsonWriter writer, Sector sector, JsonSerializerOptions options)
         {
             writer.WriteStartObject();
-            writer.WriteString(nameof(sector.UniqueId), sector.UniqueId);
-            writer.WriteString(nameof(sector.Title), sector.Title);
+            writer.WriteString(Renamer(nameof(sector.UniqueId)), sector.UniqueId);
+            writer.WriteString(Renamer(nameof(sector.Title)), sector.Title);
             writer.WriteEndObject();
+
+            string Renamer(string name) => options.PropertyNamingPolicy?.ConvertName(name) ?? name;
         }
+        
     }
 }
