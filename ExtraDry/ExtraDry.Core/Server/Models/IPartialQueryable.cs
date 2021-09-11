14b3e14e6e225301e4d8b8d5b4855ad2528c2773
Adrian Akison <adrian@akison.com>
2021-09-11 15:39:04 +1000

    Refactor interface to own file

---
+++ ExtraDry/ExtraDry.Core/Server/Models/IPartialQueryable.cs
@@ -0,0 +1,19 @@
+﻿using ExtraDry.Core;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExtraDry.Server {
+
+    public interface IPartialQueryable<T> : IQueryable<T> {
+
+        PagedCollection<T> ToPagedCollection();
+
+        Task<PagedCollection<T>> ToPagedCollectionAsync(CancellationToken cancellationToken = default);
+
+        FilteredCollection<T> ToFilteredCollection();
+
+        Task<FilteredCollection<T>> ToFilteredCollectionAsync(CancellationToken cancellationToken = default);
+
+    }
+}
--- ExtraDry/ExtraDry.Core/Server/Models/PartialQueryable.cs
+++ ExtraDry/ExtraDry.Core/Server/Models/PartialQueryable.cs
@@ -10,18 +10,6 @@ using System.Threading.Tasks;
 
 namespace ExtraDry.Server {
 
-    public interface IPartialQueryable<T> : IQueryable<T> {
-
-        PagedCollection<T> ToPagedCollection();
-    
-        Task<PagedCollection<T>> ToPagedCollectionAsync(CancellationToken cancellationToken = default);
-
-        FilteredCollection<T> ToFilteredCollection();
-
-        Task<FilteredCollection<T>> ToFilteredCollectionAsync(CancellationToken cancellationToken = default);
-
-    }
-
     public class PartialQueryable<T> : IPartialQueryable<T> {
 
         public PartialQueryable(IQueryable<T> queryable, FilterQuery filterQuery, Expression<Func<T, bool>>? defaultFilter)
