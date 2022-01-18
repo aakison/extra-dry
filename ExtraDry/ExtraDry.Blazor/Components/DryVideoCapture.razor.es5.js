eee443c809ec3baa4cf2d807e318b30a75488bd8
Adrian Akison <adrian@akison.com>
2022-01-19 00:15:02 +1000

    Add in better looking font

--- ExtraDry/ExtraDry.Blazor/Components/DryVideoCapture.razor.es5.js
+++ ExtraDry/ExtraDry.Blazor/Components/DryVideoCapture.razor.es5.js
@@ -1,53 +1,61 @@
 ﻿'use strict';
 
-function initCamera() {
-    var video, canvas, snap, errorMsgElement, constraints, stream, context;
-    return regeneratorRuntime.async(function initCamera$(context$1$0) {
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var errorMsgElement = document.querySelector('span#errorMsg');
+var stream;
+
+var constraints = {
+    audio: false,
+    video: {
+        width: 480, height: 480, facingMode: "user"
+    }
+};
+
+function startCamera() {
+    return regeneratorRuntime.async(function startCamera$(context$1$0) {
         while (1) switch (context$1$0.prev = context$1$0.next) {
             case 0:
-                video = document.getElementById('video');
-                canvas = document.getElementById('canvas');
-                snap = document.getElementById("snap");
-                errorMsgElement = document.querySelector('span#errorMsg');
-                constraints = {
-                    audio: false,
-                    video: {
-                        width: 480, height: 480
-                    }
-                };
-                context$1$0.prev = 5;
-                context$1$0.next = 8;
+                context$1$0.prev = 0;
+                context$1$0.next = 3;
                 return regeneratorRuntime.awrap(navigator.mediaDevices.getUserMedia(constraints));
 
-            case 8:
+            case 3:
                 stream = context$1$0.sent;
 
-                handleSuccess(stream);
-                context$1$0.next = 15;
+                video.srcObject = stream;
+                video.play();
+                context$1$0.next = 11;
                 break;
 
-            case 12:
-                context$1$0.prev = 12;
-                context$1$0.t0 = context$1$0['catch'](5);
+            case 8:
+                context$1$0.prev = 8;
+                context$1$0.t0 = context$1$0['catch'](0);
 
                 errorMsgElement.innerHTML = 'navigator.getUserMedia error:' + context$1$0.t0.toString();
 
-            case 15:
-                context = canvas.getContext('2d');
-
-                snap.addEventListener("click", function () {
-                    context.drawImage(video, 0, 0, 480, 480);
-                });
-
-            case 17:
+            case 11:
             case 'end':
                 return context$1$0.stop();
         }
-    }, null, this, [[5, 12]]);
+    }, null, this, [[0, 8]]);
 }
 
-function handleSuccess(stream) {
-    window.stream = stream;
-    video.srcObject = stream;
+function captureImage() {
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, 480, 480);
 }
+
+function stopCamera() {
+    stream.getTracks().forEach(function (track) {
+        return track.stop();
+    });
+}
+
+exports.startCamera = startCamera;
+exports.captureImage = captureImage;
+exports.stopCamera = stopCamera;
 
