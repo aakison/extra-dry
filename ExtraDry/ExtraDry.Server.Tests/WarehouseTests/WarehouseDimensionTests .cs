39d62f930963476704769a371b0f5d48b3655a7f
Adrian Akison <adrian@akison.com>
2022-05-13 16:36:12 +1000

    Remove dimension tests as dimensions not yet moved to builder model

--- ExtraDry/ExtraDry.Server.Tests/WarehouseTests/WarehouseDimensionTests .cs
+++ ExtraDry/ExtraDry.Server.Tests/WarehouseTests/WarehouseDimensionTests .cs
@@ -1,44 +1,44 @@
-﻿using ExtraDry.Server.DataWarehouse;
-using Microsoft.EntityFrameworkCore;
+﻿//using ExtraDry.Server.DataWarehouse;
+//using Microsoft.EntityFrameworkCore;
 
-namespace ExtraDry.Server.Tests.WarehouseTests;
+//namespace ExtraDry.Server.Tests.WarehouseTests;
 
-public class WarehouseDimensionTests {
+//public class WarehouseDimensionTests {
 
-    [Theory]
-    [InlineData(typeof(CompanyStatus))]
-    [InlineData(typeof(RegionStatus))]
-    public void EntityBecomesDimension(Type entityType)
-    {
-        var warehouse = new Warehouse();
+//    [Theory]
+//    [InlineData(typeof(CompanyStatus))]
+//    [InlineData(typeof(RegionStatus))]
+//    public void EntityBecomesDimension(Type entityType)
+//    {
+//        var warehouse = new Warehouse();
         
-        warehouse.CreateSchema(context);
+//        warehouse.CreateSchema(context);
 
-        Assert.Contains(warehouse.Dimensions, e => e.EntityType == entityType);
-    }
+//        Assert.Contains(warehouse.Dimensions, e => e.EntityType == entityType);
+//    }
 
-    [Theory]
-    [InlineData(typeof(RegionLevel))]
-    [InlineData(typeof(Company))]
-    public void EntityDoesNotBecomeDimension(Type entityType)
-    {
-        var warehouse = new Warehouse();
+//    [Theory]
+//    [InlineData(typeof(RegionLevel))]
+//    [InlineData(typeof(Company))]
+//    public void EntityDoesNotBecomeDimension(Type entityType)
+//    {
+//        var warehouse = new Warehouse();
 
-        warehouse.CreateSchema(context);
+//        warehouse.CreateSchema(context);
 
-        Assert.DoesNotContain(warehouse.Dimensions, e => e.EntityType == entityType);
-    }
+//        Assert.DoesNotContain(warehouse.Dimensions, e => e.EntityType == entityType);
+//    }
 
 
 
-    private readonly SampleContext context = GetDatabase();
+//    private readonly SampleContext context = GetDatabase();
 
-    private static SampleContext GetDatabase()
-    {
-        var options = new DbContextOptionsBuilder<SampleContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-            .Options;
-        return new SampleContext(options);
-    }
+//    private static SampleContext GetDatabase()
+//    {
+//        var options = new DbContextOptionsBuilder<SampleContext>()
+//            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+//            .Options;
+//        return new SampleContext(options);
+//    }
 
-}
+//}
