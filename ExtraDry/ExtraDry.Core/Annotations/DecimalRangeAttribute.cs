b93197522e67d8dd204ff9f0187317b5bf8ef8b0
Adrian Akison <adrian@akison.com>
2022-10-06 15:49:18 +1000

    Modernize language features.

--- ExtraDry/ExtraDry.Core/Annotations/DecimalRangeAttribute.cs
+++ ExtraDry/ExtraDry.Core/Annotations/DecimalRangeAttribute.cs
@@ -1,46 +1,42 @@
-﻿#nullable enable
+﻿namespace ExtraDry.Core;
 
-using System.ComponentModel.DataAnnotations;
-
-namespace ExtraDry.Core {
+/// <summary>
+/// Validates a decimal value as being within the given range.
+/// Functionally the same as the RangeAttribute but works for decimals.
+/// </summary>
+public class DecimalRangeAttribute : ValidationAttribute {
 
     /// <summary>
-    /// Validates a decimal value as being within the given range.
-    /// Functionally the same as the RangeAttribute but works for decimals.
+    /// The minimum, inclusive, lower bound for the value.
     /// </summary>
-    public class DecimalRangeAttribute : ValidationAttribute {
+    public decimal Minimum { get; set; }
 
-        /// <summary>
-        /// The minimum, inclusive, lower bound for the value.
-        /// </summary>
-        public decimal Minimum { get; set; }
+    /// <summary>
+    /// The maximum, inclusive, upper bound for the value.
+    /// </summary>
+    public decimal Maximum { get; set; }
 
-        /// <summary>
-        /// The maximum, inclusive, upper bound for the value.
-        /// </summary>
-        public decimal Maximum { get; set; }
-
-        /// <summary>
-        /// Create a new validator for a decimal range
-        /// </summary>
-        /// <remarks>
-        /// Constructor uses double since attibutes need CLR types.
-        /// </remarks>
-        public DecimalRangeAttribute(double minimum, double maximum)
-        {
-            Minimum = (decimal)minimum;
-            Maximum = (decimal)maximum;
-        }
-
-        /// <summary>
-        /// Standard validation override.
-        /// </summary>
-        public override bool IsValid(object? value) => 
-            value switch {
-                null => true,
-                decimal decimalValue => Minimum <= decimalValue && decimalValue <= Maximum,
-                _ => false,
-            };
-
+    /// <summary>
+    /// Create a new validator for a decimal range
+    /// </summary>
+    /// <remarks>
+    /// Constructor uses double since attibutes need CLR types.
+    /// </remarks>
+    public DecimalRangeAttribute(double minimum, double maximum)
+    {
+        Minimum = (decimal)minimum;
+        Maximum = (decimal)maximum;
     }
+
+    /// <summary>
+    /// Standard validation override.
+    /// </summary>
+    public override bool IsValid(object? value) => 
+        value switch {
+            null => true,
+            decimal decimalValue => Minimum <= decimalValue && decimalValue <= Maximum,
+            _ => false,
+        };
+
 }
+
