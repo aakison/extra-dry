8031d725aff53340ff4cca225c2cf0298b306301
Adrian Akison <adrian@akison.com>
2023-02-14 15:28:45 +1000

    Basic roundtrip tests for attributes

---
+++ ExtraDry/ExtraDry.Core.Tests/Annotations/EntityMessageAttributeTests.cs
@@ -0,0 +1,34 @@
+﻿using ExtraDry.Core.Models;
+
+namespace ExtraDry.Core.Tests;
+
+public class EntityMessageTests {
+
+    [Theory]
+    [InlineData("EntityName", "abc")]
+    [InlineData("EntityName", "")]
+    public void RoundtripFormatProperty(string propertyName, string propertyValue)
+    {
+        var entityMessage = new EntityMessage(propertyValue);
+        var property = entityMessage.GetType().GetProperty(propertyName);
+
+        var result = property?.GetValue(entityMessage);
+
+        Assert.Equal(propertyValue, result);
+    }
+
+    [Theory]
+    [InlineData("EntityName", "abc")]
+    [InlineData("EntityName", "")]
+    public void RoundtripEntityMessageProperty(string propertyName, string propertyValue)
+    {
+        var entityMessage = new EntityMessage("different");
+        var property = entityMessage.GetType().GetProperty(propertyName);
+
+        property?.SetValue(entityMessage, propertyValue);
+        var result = property?.GetValue(entityMessage);
+
+        Assert.Equal(propertyValue, result);
+    }
+
+}
