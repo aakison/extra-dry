f97684ac93f520f2e7823783b9eab1b651df2135
Adrian Akison <adrian@akison.com>
2023-04-24 11:34:38 +1000

    Add documentation

--- ExtraDry/ExtraDry.Core/RuleAction.cs
+++ ExtraDry/ExtraDry.Core/RuleAction.cs
@@ -1,37 +1,35 @@
-﻿using System.Text.Json.Serialization;
-
-namespace ExtraDry.Core;
+﻿namespace ExtraDry.Core;
 
 /// <summary>
-/// Actions that are used by rules to determine how creates and updates should be processed.
+/// Actions that are used by the Rule Engine to determine how creates and updates should be processed.
 /// </summary>
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum RuleAction {
 
     /// <summary>
-    /// Changes to values will always be allowed by `RuleEngine`.
+    /// Changes to values will always be allowed by the Rule Engine.
     /// </summary>
     Allow,
 
     /// <summary>
-    /// Changes to values will always be ignored by `RuleEngine`.
+    /// Changes to values will always be ignored by the Rule Engine.
     /// </summary>
     Ignore,
 
     /// <summary>
-    /// Values of `default` or `null` will be ignored, others will be allowed by `RuleEngine`.
+    /// Values of `default` or `null` will be ignored, others will be allowed by the Rule Engine.
     /// </summary>
     IgnoreDefaults,
 
     /// <summary>
-    /// Any attempt to change a value will raise an exception by the `RuleEngine`.
+    /// Any attempt to change a value will raise an exception by the Rule Engine.
     /// Values of `default` or `null` do not trigger the exception.
     /// </summary>
     Block,
 
     /// <summary>
-    /// Incoming values are not copied, instead the matching `IEntityResolver` is
-    /// used to lookup the existing entity to link to.
+    /// Incoming values are not copied, instead the matching Entity Resolver (implements 
+    /// `IEntityResolver`) is used to lookup the existing entity to link to.
     /// </summary>
     Link,
 
--- ExtraDry/ExtraDry.Core/RulesAttribute.cs
+++ ExtraDry/ExtraDry.Core/RulesAttribute.cs
@@ -1,24 +1,36 @@
 ﻿namespace ExtraDry.Core;
 
 /// <summary>
-/// Declare on properties to instruct the `RuleEngine` how to address the creating, updating and deleting of the property.
+/// Declare on properties to instruct the Rule Engine how to address the creating and updating of the property.
 /// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class RulesAttribute : Attribute {
 
+    /// <summary>
+    /// Create a Rule that allows property to change on both create and update.
+    /// </summary>
     public RulesAttribute()
     {
 
     }
 
+    /// <summary>
+    /// Create a Rule that has the same action for both create and update.
+    /// </summary>
     public RulesAttribute(RuleAction defaultRule)
     {
         UpdateAction = defaultRule;
         CreateAction = defaultRule;
     }
 
+    /// <summary>
+    /// The action that should be take by the Rule Engine during create.
+    /// </summary>
     public RuleAction CreateAction { get; set; } = RuleAction.Allow;
 
+    /// <summary>
+    /// The action that should be take by the Rule Engine during update.
+    /// </summary>
     public RuleAction UpdateAction { get; set; } = RuleAction.Allow;
 
 }
