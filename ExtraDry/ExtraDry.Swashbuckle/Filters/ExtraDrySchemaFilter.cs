da174bf92d4f580cbbad50b2e59f800d0ee1b24b
Adrian Akison <adrian@akison.com>
2023-04-24 11:35:17 +1000

    Make ReadOnly schema more specific for both update and create as OpenAPI doesn't differentiate

--- ExtraDry/ExtraDry.Swashbuckle/Filters/ExtraDrySchemaFilter.cs
+++ ExtraDry/ExtraDry.Swashbuckle/Filters/ExtraDrySchemaFilter.cs
@@ -2,12 +2,31 @@
 
 namespace ExtraDry.Swashbuckle;
 
+/// <summary>
+/// Updates the OpenAPI schema to apply ReadOnly to properties that can't have data posted back for
+/// either a Create or an Update.  Automatically applied when using `openapi.AddExtraDry()` inside
+/// of the `AddSwaggerGen` call in Startup.
+/// </summary>
+/// <remarks>
+/// Options for schema are ReadOnly or WriteOnly.  Determines if Swagger UI shows values on 
+/// retrieve (WriteOnly = false), or as available for POST/PUT (ReadOnly = false).
+/// 
+/// Nothing in the system would currently indicate a use for WriteOnly.  
+/// 
+/// ReadOnly set to true does make sense for fields such as Audit and Revision fields. However, can
+/// be mistakenly applied to other unchanging fields such as Tenant ID or UUID.
+/// 
+/// If a property is Ignored or Blocked for _both_ Create and Update, then it is by definition
+/// ReadOnly.  Any toher combinations allow for a change sometimes so can't be ReadOnly.
+/// </remarks>
 public class ExtraDrySchemaFilter : ISchemaFilter {
 
+    /// <inheritdoc cref="ISchemaFilter.Apply(OpenApiSchema, SchemaFilterContext)" />
     public void Apply(OpenApiSchema schema, SchemaFilterContext context)
     {
         var rule = context?.MemberInfo?.GetCustomAttribute<RulesAttribute>();
-        if(rule?.UpdateAction == RuleAction.Ignore || rule?.UpdateAction == RuleAction.Block) {
+        if((rule?.CreateAction == RuleAction.Ignore || rule?.CreateAction == RuleAction.Block) && 
+            (rule?.UpdateAction == RuleAction.Ignore || rule?.UpdateAction == RuleAction.Block)) {
             schema.ReadOnly = true;
         }
     }
