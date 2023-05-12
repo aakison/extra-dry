1a7ed5fb45236b56f6a85dc4a3f8606af2dfd3dc
Antonio Pasquale <antonio.pasquale@fmiworks.com>
2023-05-12 12:36:14 +1000

    Added Spinner Component

---
+++ ExtraDry/ExtraDry.Blazor/Components/Standard/Spinner.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtraDry.Blazor; 
+/// <summary>
+/// Represents a simple styled spinning circle which can be used to represent
+/// a value or item in a loading state
+/// </summary>
+public partial class Spinner : ComponentBase, IExtraDryComponent {
+    /// <inheritdoc cref="IExtraDryComponent.CssClass" />
+    [Parameter]
+    public string CssClass { get; set; } = string.Empty;
+
+    /// <inheritdoc cref="IExtraDryComponent.UnmatchedAttributes" />
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object> UnmatchedAttributes { get; set; } = null!;
+
+    private string CssClasses => DataConverter.JoinNonEmpty(" ", CssClass, "spinner");
+}
