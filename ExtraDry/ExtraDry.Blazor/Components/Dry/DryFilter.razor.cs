c97190ebe89a3d36e735c3b7afd174fc2736ebde
Adrian Akison <adrian@akison.com>
2023-06-12 23:41:01 +1000

    Make filter control have list of filters to show/hide specific filters

--- ExtraDry/ExtraDry.Blazor/Components/Dry/DryFilter.razor.cs
+++ ExtraDry/ExtraDry.Blazor/Components/Dry/DryFilter.razor.cs
@@ -5,6 +5,10 @@ public partial class DryFilter<TItem> : ComponentBase, IExtraDryComponent {
     public DryFilter()
     {
         ViewModelDescription = new ViewModelDescription(typeof(TItem), this);
+        AllFilters = new List<string> { KeywordsFitlerIdentifier };
+        AllFilters.AddRange(ViewModelDescription.FilterProperties
+            .Where(e => e.HasDiscreteValues)
+            .Select(e => e.Property.Name));
     }
 
     /// <inheritdoc cref="IExtraDryComponent.CssClass "/>
@@ -22,8 +26,33 @@ public partial class DryFilter<TItem> : ComponentBase, IExtraDryComponent {
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> UnmatchedAttributes { get; set; } = null!;
 
+    protected void DoFiltersSubmit(DialogEventArgs _)
+    {
+        DisplayedFilters = SelectedFilters.ToList();
+    }
+
+    protected void DoFiltersCancel(DialogEventArgs _)
+    {
+        SelectedFilters = DisplayedFilters.ToList();
+    }
+
+    private List<string> AllFilters { get; }
+
+    private List<string> SelectedFilters { get; set; } = new();
+
+    private List<string> DisplayedFilters { get; set; } = new();
+
+    private IEnumerable<PropertyDescription> DisplayedEnumFilters => ViewModelDescription.FilterProperties
+        .Where(e => e.HasDiscreteValues && IsFilterSelected(e.Property.Name));
+
+    private bool DisplayKeywordFilter => IsFilterSelected(KeywordsFitlerIdentifier);
+
+    private bool IsFilterSelected(string name) =>
+        !DisplayedFilters.Any() || DisplayedFilters.Contains(name);
+
     private ViewModelDescription ViewModelDescription { get; set; }
 
     private string CssClasses => DataConverter.JoinNonEmpty(" ", "dry-filter", CssClass);
 
+    private const string KeywordsFitlerIdentifier = "Keywords";
 }
