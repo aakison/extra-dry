874deed712d5c4516777df5963177fd7db312f64
Antonio Pasquale <antonio.pasquale@fmiworks.com>
2023-06-23 15:29:47 +1000

    All stages are now RenderFragment templates that can be overridden by the consumer

--- ExtraDry/ExtraDry.Blazor/Components/Standard/ValueLoader.razor.cs
+++ ExtraDry/ExtraDry.Blazor/Components/Standard/ValueLoader.razor.cs
@@ -1,6 +1,4 @@
-﻿using ExtraDry.Core;
-
-namespace ExtraDry.Blazor;
+﻿namespace ExtraDry.Blazor;
 /// <summary>
 /// Represents a value that requires an async operation to populate.
 /// Can be in the following states:
@@ -9,22 +7,42 @@ namespace ExtraDry.Blazor;
 /// - Timeout
 /// - Complete - Loaded with Value
 /// </summary>
-public partial class ValueLoader : ComponentBase, IExtraDryComponent {
+public partial class ValueLoader<ValueModel> : ComponentBase, IExtraDryComponent {
+    /// <summary>
+    /// Render Fragment for when the value has been loaded. This will govern how the value is displayed
+    /// </summary>
+    [Parameter]
+    public RenderFragment<ValueLoaderContext>? Display { get; set; }
+    /// <summary>
+    /// Render Fragment for when an error is encountered during loading.
+    /// A default is provided but this can be used to override the display
+    /// </summary>
+    [Parameter]
+    public RenderFragment<ValueLoaderContext>? Error { get; set; }
+    /// <summary>
+    /// Render Fragment for when a timeout is encountered during loading.
+    /// A default is provided but this can be used to override the display
+    /// </summary>
+    [Parameter]
+    public RenderFragment<ValueLoaderContext>? Timeout { get; set; }
+    /// <summary>
+    /// Render Fragment for when a value is in the process of being loaded.
+    /// A default is provided but this can be used to override the display
+    /// </summary>
+    [Parameter]
+    public RenderFragment<ValueLoaderContext>? Loading { get; set; }
     /// <inheritdoc cref="IExtraDryComponent.CssClass" />
     [Parameter]
     public string CssClass { get; set; } = string.Empty;
-
-    [Parameter]
     /// <summary>
     /// Delegate function for how to retrieve it's Value 
     /// Requires a method that is async and returns Task&lt;object?$gt;
     /// </summary>
-    public Func<Task<object?>>? LoadData { get; set; }
-
+    [Parameter]    
+    public Func<Task<ValueModel?>>? LoadData { get; set; }
     /// <inheritdoc cref="IExtraDryComponent.UnmatchedAttributes" />
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> UnmatchedAttributes { get; set; } = null!;
-
     /// <inheritdoc cref="SpinnerSize" />
     [Parameter]
     public SpinnerSize Size { get; set; } = SpinnerSize.Standard;
@@ -32,11 +50,10 @@ public partial class ValueLoader : ComponentBase, IExtraDryComponent {
     /// <summary>
     /// The value once loaded
     /// </summary>
-    public object? Value { get; set; }
+    public ValueModel? Value { get; set; }
 
-    public LoadingState State { get; set; }
-
-    public string StateMessage { get; set; } = string.Empty;    
+    /// <inheritdoc cref="LoadingState" />
+    public LoadingState State { get; set; }  
 
     private string CssClasses => DataConverter.JoinNonEmpty(" ", CssClass, "value-loader", State.ToString().ToLower());
 
@@ -51,7 +68,7 @@ public partial class ValueLoader : ComponentBase, IExtraDryComponent {
 
         if(LoadData == null) {
             State = LoadingState.Error;
-            StateMessage = "No data to load";
+            Console.WriteLine("Error: No LoadData method passed");
             return;
         }
 
@@ -69,7 +86,6 @@ public partial class ValueLoader : ComponentBase, IExtraDryComponent {
         }
         catch(Exception ex) {
             State = LoadingState.Error;
-            StateMessage = "An error has occurred";
             Console.WriteLine(ex.ToString());
         }
     }
@@ -78,11 +94,45 @@ public partial class ValueLoader : ComponentBase, IExtraDryComponent {
     {
         await DoLoadData();
     }
+
+    /// <summary>
+    /// Context passed through to the child components
+    /// </summary>
+    public class ValueLoaderContext {
+        /// <summary>
+        /// The loaded data value
+        /// </summary>
+        public ValueModel? Value { get; set; }
+        /// <summary>
+        /// The size of the loading icon
+        /// </summary>
+        public SpinnerSize Size { get; set; }
+        /// <summary>
+        /// A callback method to retry the load process
+        /// </summary>
+        public Func<Task>? Reload { get; set; }
+    }
 }
 
+/// <summary>
+/// Defines the state the ValueLoader is in
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum LoadingState {
+    /// <summary>
+    /// Value is still loading
+    /// </summary>
     Loading,
+    /// <summary>
+    /// An error has been encountered during loading
+    /// </summary>
     Error,
+    /// <summary>
+    /// An timeout has been encountered during loading
+    /// </summary>
     Timeout,
+    /// <summary>
+    /// The value has been loaded
+    /// </summary>
     Complete
 }
