efd2a7e64139f63fea580a53f7196a63337cdadd
Adrian Akison <adrian@akison.com>
2023-06-27 18:18:45 +1000

    Remove access to Resource to test it working as a Blazor package

--- ExtraDry/ExtraDry.Highlight/Configuration/DefaultConfiguration.cs
+++ ExtraDry/ExtraDry.Highlight/Configuration/DefaultConfiguration.cs
@@ -6,6 +6,7 @@ public class DefaultConfiguration : XmlConfiguration
 {
     public DefaultConfiguration()
     {
-        XmlDocument = XDocument.Parse(Resources.DefaultDefinitions);
+        //XmlDocument = XDocument.Parse(Resources.DefaultDefinitions);
+        XmlDocument = XDocument.Parse(HardcodedDefinitions.Xml.TrimStart());
     }
 }
