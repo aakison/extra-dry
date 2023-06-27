bb58eb134db61f8a5bb89c37f08070d3ce5d635a
Adrian Akison <adrian@akison.com>
2023-06-27 13:25:48 +1000

    Clean up some coding patterns inherited from highlighter

--- ExtraDry/ExtraDry.Highlight/Patterns/BlockPattern.cs
+++ ExtraDry/ExtraDry.Highlight/Patterns/BlockPattern.cs
@@ -5,10 +5,6 @@ namespace ExtraDry.Highlight;
 
 public sealed class BlockPattern : Pattern
 {
-    public string BeginsWith { get; private set; }
-    public string EndsWith { get; private set; }
-    public string EscapesWith { get; private set; }
-
     public BlockPattern(string name, Style style, string beginsWith, string endsWith, string escapesWith)
         : base(name, style)
     {
@@ -16,18 +12,23 @@ public sealed class BlockPattern : Pattern
         EndsWith = endsWith;
         EscapesWith = escapesWith;
     }
+    public string BeginsWith { get; private set; }
+
+    public string EndsWith { get; private set; }
+
+    public string EscapesWith { get; private set; }
 
     public override string GetRegexPattern()
     {
-        if (String.IsNullOrEmpty(EscapesWith)) {
+        if (string.IsNullOrEmpty(EscapesWith)) {
             if (EndsWith.CompareTo(@"\n") == 0) {
-                return String.Format(@"{0}[^\n\r]*", Escape(BeginsWith));
+                return string.Format(@"{0}[^\n\r]*", Escape(BeginsWith));
             }
 
-            return String.Format(@"{0}[\w\W\s\S]*?{1}", Escape(BeginsWith), Escape(EndsWith));
+            return string.Format(@"{0}[\w\W\s\S]*?{1}", Escape(BeginsWith), Escape(EndsWith));
         }
 
-        return String.Format("{0}(?>{1}.|[^{2}]|.)*?{3}", new object[] { Regex.Escape(BeginsWith), Regex.Escape(EscapesWith.Substring(0, 1)), Regex.Escape(EndsWith.Substring(0, 1)), Regex.Escape(EndsWith) });
+        return string.Format("{0}(?>{1}.|[^{2}]|.)*?{3}", new object[] { Regex.Escape(BeginsWith), Regex.Escape(EscapesWith.Substring(0, 1)), Regex.Escape(EndsWith.Substring(0, 1)), Regex.Escape(EndsWith) });
     }
 
     public static string Escape(string str)
