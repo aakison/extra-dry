d78339f7c05ab632929d1c20a677750d60029a1c
Jaspal <jmuker@hotmail.com>
2023-09-21 09:16:30 +1000

    Fixed ExpandoValues namespaces

--- ExtraDry/ExtraDry.Core/ExpandoData/ExpandoValues.cs
+++ ExtraDry/ExtraDry.Core/ExpandoData/ExpandoValues.cs
@@ -1,21 +1,20 @@
 ﻿using System.Text.Json;
 
-namespace ExtraDry.Core.Templates {
+namespace ExtraDry.Core; 
 
-    [JsonConverter(typeof(ExpandoValuesConverter))]
-    public class ExpandoValues : IValidatableObject {
+[JsonConverter(typeof(ExpandoValuesConverter))]
+public class ExpandoValues : IValidatableObject {
 
-        public Dictionary<string, object> Values { get; set; } = new();
+    public Dictionary<string, object> Values { get; set; } = new();
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Values.Values.Any(e => e is JsonElement && ((JsonElement)e).ValueKind == JsonValueKind.Array)) {
-                yield return new ValidationResult("Arrays are not allowed as custom values.");
-            }
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if(Values.Values.Any(e => e is JsonElement && ((JsonElement)e).ValueKind == JsonValueKind.Array)) {
+            yield return new ValidationResult("Arrays are not allowed as custom values.");
+        }
 
-            if(Values.Values.Any(e => e is JsonElement && ((JsonElement)e).ValueKind == JsonValueKind.Object)) {
-                yield return new ValidationResult("Complex Objects are not allowed as custom values.");
-            }
+        if(Values.Values.Any(e => e is JsonElement && ((JsonElement)e).ValueKind == JsonValueKind.Object)) {
+            yield return new ValidationResult("Complex Objects are not allowed as custom values.");
         }
     }
 }
--- ExtraDry/ExtraDry.Core/ExpandoData/ExpandoValuesConverter.cs
+++ ExtraDry/ExtraDry.Core/ExpandoData/ExpandoValuesConverter.cs
@@ -1,23 +1,21 @@
-﻿using ExtraDry.Core.Templates;
-using System.Text.Json;
+﻿using System.Text.Json;
 
-namespace ExtraDry.Core {
+namespace ExtraDry.Core;
 
-    public class ExpandoValuesConverter : JsonConverter<ExpandoValues> {
+public class ExpandoValuesConverter : JsonConverter<ExpandoValues> {
 
-        public override ExpandoValues? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            var fieldsDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(ref reader, options);
-            if(fieldsDictionary == null) {
-                return default;
-            }
-
-            return new ExpandoValues { Values = fieldsDictionary };
+    public override ExpandoValues? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var fieldsDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(ref reader, options);
+        if(fieldsDictionary == null) {
+            return default;
         }
 
-        public override void Write(Utf8JsonWriter writer, ExpandoValues value, JsonSerializerOptions options)
-        {
-            JsonSerializer.Serialize(writer, value.Values, options);
-        }
+        return new ExpandoValues { Values = fieldsDictionary };
+    }
+
+    public override void Write(Utf8JsonWriter writer, ExpandoValues value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, value.Values, options);
     }
 }
