3a1be9831624cbdf1fb1c7a04d77f0e1c911023c
Hai Vu <hai.vu@fmiworks.com>
2023-09-22 11:41:11 +1000

    Add new CodeSlugAttribute and TitleSlugAttribute for working with slugs.

---
+++ ExtraDry/ExtraDry.Core/Annotations/TitleSlugAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtraDry.Core;
+
+/// <summary>
+/// Validates that a string field is a valid Slug which is being used as a URL friendly unique ID.
+/// This only allows lowercase letters, numbers, and hyphens.
+/// </summary>
+public class TitleSlugAttribute : RegularExpressionAttribute {
+
+    /// <inheritdoc cref="TitleSlugAttribute" />
+    public TitleSlugAttribute() : base(TitleSlugRegex)
+    {
+        ErrorMessage = "The {0} field should use only lowercase letters, numbers and hyphens.";
+    }
+
+    private const string TitleSlugRegex = "^[a-z0-9-]+$";
+}
+
