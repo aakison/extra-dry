bc137c7d40e09541091a64420d08e0c621ee1d6c
Jaspal <jmuker@hotmail.com>
2023-10-10 08:33:57 +1000

    Comment Cleanup

--- ExtraDry/ExtraDry.Core/Annotations/ValidIdentifierAttribute.cs
+++ ExtraDry/ExtraDry.Core/Annotations/ValidIdentifierAttribute.cs
@@ -5,7 +5,6 @@
 /// </summary>
 public class ValidIdentifierAttribute : RegularExpressionAttribute {
 
-    /// <inheritdoc cref="TitleSlugAttribute" />
     public ValidIdentifierAttribute() : base(ValidIdentifierRegex)
     {
         ErrorMessage = "The {0} field can only contain unicode letters, $, _ and digits (0-9) but may not start with a digit.";
