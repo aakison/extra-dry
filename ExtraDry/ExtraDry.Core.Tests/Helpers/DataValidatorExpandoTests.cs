c4eb4be94166c91c4e1b72739d798cc6cdcaf1a5
Adrian Akison <adrian@akison.com>
2023-10-09 11:05:13 +1000

    Add some basic additional tests for data validation, not complete to all options

---
+++ ExtraDry/ExtraDry.Core.Tests/Helpers/DataValidatorExpandoTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExtraDry.Core.Tests.Helpers;
+
+public class DataValidatorExpandoTests {
+
+    [Fact]
+    public void EmptyExpandoValidates()
+    {
+        var validator = new DataValidator();
+        var sample = SchemaSample;
+
+        var success = validator.ValidateObject(sample);
+
+        Assert.True(success);
+        Assert.Empty(validator.Errors);
+    }
+
+    [Theory]
+    [InlineData("ASX_CODE", "OK")]
+    //[InlineData("ABN", "012345678")]
+    public void ValidValue(string key, object value)
+    {
+        var validator = new DataValidator();
+        var sample = SchemaSample;
+        sample.Values[key] = value;
+
+        var success = validator.ValidateObject(sample);
+
+        Assert.True(success);
+        Assert.Empty(validator.Errors);
+    }
+
+    [Theory]
+    [InlineData("ASX_CODE", "OK0123456789")] // MaxLength
+    //[InlineData("ABN", "")] // Required
+    public void InalidValue(string key, object value)
+    {
+        var validator = new DataValidator();
+        var sample = SchemaSample;
+        sample.Values[key] = value;
+
+        var success = validator.ValidateObject(sample);
+
+        Assert.False(success);
+        Assert.Equal(1, validator.Errors.Count);
+    }
+
+    public class Sample {
+
+        public ExpandoValues Values { get; set; } = new();
+
+    }
+
+    public ExpandoSchema Schema => new ExpandoSchema {
+        TargetType = typeof(Sample).Name,
+        Fields = new List<ExpandoField> {
+            //TODO: this requires more design, how to make required field when field input is optional???
+            //new() { DataType = ExpandoDataType.Text, Slug = "ABN", MaxLength = 10, IsRequired = true },
+            new() { DataType = ExpandoDataType.Text, Slug = "ASX_CODE", MaxLength = 10, IsRequired = false }
+        }
+    };
+
+    public Sample SchemaSample {
+        get {
+            var sample = new Sample();
+            sample.Values.Schema = Schema;
+            return sample;
+        }   
+    }
+
+}
