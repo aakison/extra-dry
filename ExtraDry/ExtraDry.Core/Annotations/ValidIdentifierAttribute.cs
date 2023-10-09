64a7a39b1d1d686aac11226c35953edd5b67cfad
Jaspal <jmuker@hotmail.com>
2023-10-09 15:35:05 +1000

    Autogenerate Field slug from Label

---
+++ ExtraDry/ExtraDry.Core/Annotations/ValidIdentifierAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtraDry.Core;
+
+/// <summary>
+/// A valid identifier can contain unicode letters, $, _ and digits (0-9) but may not start with a digit.
+/// </summary>
+public class ValidIdentifierAttribute : RegularExpressionAttribute {
+
+    /// <inheritdoc cref="TitleSlugAttribute" />
+    public ValidIdentifierAttribute() : base(ValidIdentifierRegex)
+    {
+        ErrorMessage = "The {0} field can only contain unicode letters, $, _ and digits (0-9) but may not start with a digit.";
+    }
+
+    private const string ValidIdentifierRegex = "^[_\\$a-zA-Z]\\w+$";
+}
+
