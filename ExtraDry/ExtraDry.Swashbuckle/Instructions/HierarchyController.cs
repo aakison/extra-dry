f073caa825f7cacf2154647bdbd8cf8d366308ae
Adrian Akison <adrian@akison.com>
2023-11-02 20:16:42 +1000

    Add hierarchy controller instructions content

--- ExtraDry/ExtraDry.Swashbuckle/Instructions/HierarchyController.cs
+++ ExtraDry/ExtraDry.Swashbuckle/Instructions/HierarchyController.cs
@@ -5,53 +5,85 @@ using System.Diagnostics.CodeAnalysis;
 namespace ExtraDry.Swashbuckle.Instructions;
 
 /// <summary>
-/// TODO: Intro:
-/// When listing entities, sometimes the number of entities returned is too large for a single API
-/// call.  This could be because of size limits.  It could also be because of user constraints.
-/// Regardless, when APIs may return large sets, they'll usually have a paging mechanism.  
-///
-/// When paging is required, it always comes in two flavors.  A skip-take variant and a continuation
-/// token variant.
+/// Some data entities naturally occur in a hierarchy which is often represented as a tree. This 
+/// presents some unique challenges when it comes to querying and displaying the data.  For these 
+/// entities, an additional API endpoint is provided.
 /// 
-/// ### Capabilities
-/// * Level only
-/// * Level plus expand
-/// * Level plus collapse
-/// * Any number, so gets big and endpoints likely to be POST
-/// * Filter respects level, includes ancestors
-/// * Ancestors overrides collapse
-/// * Often will support paging, but not tokens.
+/// The hierarchy endpoint provides the common interaction mechanisms for working with entities as
+/// a tree in addition to treating it as a list.  This includes a different query mechanism to 
+/// collect a tree subset, as well as a guaranteed depth-first ordering which suits row based 
+/// display to users.
 /// 
-/// The conceptually simpler mechanism is that of a skip-take.  You may `skip` as many records as
-/// you want and then request to `take` as many as works for your interface.  This mechanism is more
-/// typically used when the result of the query is being displayed to a user.  Users can only see
-/// and process a limited view, so it makes sense to track where they are just show them a changing
-/// subset.
-///
-/// For a page-based interface, set your `take` to the size of your page.  Then, set your `skip` to
-/// the index of the first record on that page.  For example, if you're displaying 25 records per
-/// page and are showing page 3, then set `skip` to `75` and `take` to `25`.
-///
-/// For a continuous-scroll interface, load in the first page of records and take note of the
-/// `total` number of entities.  Then, set the size of the scrollbar to the `total`.  Figure out how
-/// many records fit on a screen and set this to the window of the scrollbar and your `take`
-/// setting.  As the user scrolls, re-request the visible page by sending the index of the scrollbar
-/// as the `skip`.  Of course, this glosses over some details and an efficient implementation will
-/// load records before and after the current window as well as some sort of client-side caching
-/// mechanism.
-///
-/// This mechanism, however, has some drawbacks.  For example, keeping track of which records you've
-/// accessed if you're trying to access all records.  Note also, the API does not guarantee that the
-/// number of records you request in `take` will be returned.
+/// ### Queries
+/// When requesting trees of data, the query mechanism takes levels, nodes to expand, nodes to
+/// collapse, and arbitrary filter conditions.
 /// 
+/// #### Level Query
+/// The level query is the simplest query mechanism and limits the number of levels returned.  This
+/// can be used alone or in conjunction with other query mechanisms to limit the scope of their
+/// return results.  
+/// 
+/// This can support user interfaces where the entire tree can be expanded and collapsed an entire
+/// level at a time.
+/// 
+/// E.g. `?level=1` will return the first two levels of animals in the sample below (levels are 0
+/// indexed).
+/// 
+/// #### Expand Query
+/// If provided, this is an array of node identifiers that should be expanded.  The transient 
+/// identifier, Slug, is used for this operation.  This will add to the result set all children of
+/// the provided nodes.  
+/// 
+/// This can support user interfaces where individual nodes can be expanded.
+/// 
+/// E.g. `?level=1&amp;expand=vertebrates` will expand vertebrates but leave invertebrates collapsed.
+/// 
+/// #### Collapse Query
+/// If provide, this is an array of node identifiers that should be collapsed.  The transient
+/// identifier, Slug, is used for this operation.  This will remove the children of this node from
+/// being displayed in the result set, even if they would otherwise be included in a Level, Expand, 
+/// or Filter query.
+/// 
+/// This can support user interfaces where individual nodes can be collapsed.
+/// 
+/// E.g. `?level=2&amp;collapse=invertebrates` will collapse invertabrates but leave vertebrates 
+/// expanded, return the same results as the example above.
+/// 
+/// #### Filter Query
+/// The most complex, and most computationally expensive, query mechanism is the filter.  This uses
+/// the same filter query as the general filter mechanism and is applied to the tree within the 
+/// number of levels requests.  To filter the entire tree, set the Level to a number greater than
+/// the depth of the tree.
+/// 
+/// When applied with a hierarchy, it is also necessary that the filtered nodes return all their 
+/// ancestors.
+/// 
+/// This can support user interfaces where trees can be filtered and still shown as trees.  In many
+/// instances, this is confusing for users and using the list query mechanism may be more 
+/// appropriate and is faster.
+/// 
+/// E.g. `?level=100&amp;filter=elephant` will return the 'animal', 'vertebrates', and 'mammal' 
+/// nodes in addition to the 'elephant' parent node.  While `?level=2&amp;filter=elephant` will 
+/// return nothing since the node is pruned by the level query.
+/// 
+/// Note: It is possible that a collapse node will conflict with the ancestors of a filter node,
+/// in these cases the node will be included and the collapse query will be ignored.
+///
 /// ### Response Body
-/// TODO:
+/// The response body is a container of both items and metadata about the request.  In particular,
+/// it contains:
+///   * `created` - a ISO8601 formatted date with the UTC time the query was run.
+///   * `filter` - (optional) if a filter was provided for the query, the text of the filter 
+///     as requested.
+///   * `count` - the number of items returned (can be calculated, but reiterated for convenience).
+///   * `items` - an array of entities, serialization varies by endpoint. 
+///   * `level` - the requested level limit from the query.
+///   * `expand` - (optional) the requested nodes to expand from the query.
+///   * `collapse` - (optional) the requested nodes to collapse from the query.
 ///  
 /// ### Try It Out
-/// TODO:
-/// Use the endpoint below to test some paging against a sample database of car make and models - no
-/// authentication required.  The sample set is small, so the server limits the maximum `take` size
-/// to 10 records.
+/// Use the endpoint below to test some hierarchy queries against a sample database taken from the
+/// animal kingdom - no authentication required.  
 /// </summary>
 [ApiController]
 [ApiExplorerSettings(GroupName = SwaggerOptionsExtensions.GroupName)]
