adf21d6e7792454e5a74ac63451472bc62f93009
Wayne <wayne.scott@outlook.com>
2023-11-15 08:21:31 +1100

    Make CrudService args obsolete and maintain backwards compatibility.

--- ExtraDry/ExtraDry.Blazor/ViewModels/CrudService.cs
+++ ExtraDry/ExtraDry.Blazor/ViewModels/CrudService.cs
@@ -11,7 +11,7 @@ namespace ExtraDry.Blazor;
 /// The entity of Type `T must be JSON serializable and accepted by the server.
 /// On non-success (2xx) results, the service endpoints should return a ProblemDetails (RFC7807)
 /// response body.  This body will be unwrapped and throw in the body of a DryException.  If 
-/// ProblemDetails are not present, then a trivial attempt to unpacke the arbitrary response 
+/// ProblemDetails are not present, then a trivial attempt to unpack the arbitrary response 
 /// payload will be made.
 /// </summary>
 public class CrudService<T> {
@@ -32,18 +32,37 @@ public class CrudService<T> {
     /// This allows for version numbers, tenant names, etc. to be added.
     /// </param>
     /// <param name="iLogger">An optional Logger</param>
+    [Obsolete("Use Options")]
     public CrudService(HttpClient client, string collectionEndpointTemplate, ILogger<CrudService<T>>? iLogger = null)
     {
         http = client;
+        Options = new CrudServiceOptions { CrudEndpoint = collectionEndpointTemplate };
         ApiTemplate = collectionEndpointTemplate;
         logger = iLogger;
     }
 
     /// <summary>
+    /// Create a CRUD service with the specified configuration.  This service should not be 
+    /// manually added to the IServiceCollection.  Instead, use the AddCrudService`T 
+    /// extension method.
+    /// </summary>
+    public CrudService(HttpClient client, CrudServiceOptions options, ILogger<CrudService<T>> logger)
+    {
+        http = client;
+        Options = options;
+        this.logger = logger;
+        ApiTemplate = options.CrudEndpoint;
+    }
+
+    /// <summary>
     /// The API Template used to determine the final endpoint URI.
     /// </summary>
+    [Obsolete("Use from Options")]
     public string ApiTemplate { get; set; }
 
+    public CrudServiceOptions Options { get; }
+
+    [Obsolete("Inject arguments into HtttpClient derived type")]
     public async Task CreateAsync(T item, params object[] args)
     {
         var json = JsonSerializer.Serialize(item);
@@ -55,6 +74,18 @@ public class CrudService<T> {
         logger?.LogDebug("Created '{entity}' on '{endpoint}' with content: {content}", nameof(T), endpoint, json);
     }
 
+    public async Task CreateAsync(T item, CancellationToken cancellationToken = default)
+    {
+        var json = JsonSerializer.Serialize(item);
+        using var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var endpoint = ApiEndpoint(nameof(CreateAsync), string.Empty);
+        logger?.LogInformation("Created '{entity}' at '{endpoint}'", nameof(T), endpoint);
+        var response = await http.PostAsync(endpoint, content, cancellationToken);
+        await response.AssertSuccess();
+        logger?.LogDebug("Created '{entity}' on '{endpoint}' with content: {content}", nameof(T), endpoint, json);
+    }
+
+    [Obsolete("Inject arguments into HtttpClient derived type")]
     public async Task<T?> RetrieveAsync(object key, params object[] args)
     {
         var endpoint = ApiEndpoint(nameof(RetrieveAsync), key, args);
@@ -66,6 +97,18 @@ public class CrudService<T> {
         return item;
     }
 
+    public async Task<T?> RetrieveAsync(object key, CancellationToken cancellationToken = default)
+    {
+        var endpoint = ApiEndpoint(nameof(RetrieveAsync), key);
+        logger?.LogInformation("Retrieving '{entity}' from '{endpoint}'", nameof(T), endpoint);
+        var response = await http.GetAsync(endpoint, cancellationToken);
+        await response.AssertSuccess();
+        var item = await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+        logger?.LogDebug("Retrieved '{entity}' from '{endpoint}' with content: {content}", nameof(T), endpoint, item);
+        return item;
+    }
+
+    [Obsolete("Inject arguments into HtttpClient derived type")]
     public async Task UpdateAsync(object key, T item, params object[] args)
     {
         var endpoint = ApiEndpoint(nameof(UpdateAsync), key, args);
@@ -75,6 +118,16 @@ public class CrudService<T> {
         logger?.LogDebug("Updated '{entity}' on '{endpoint}' with content: {content}", nameof(T), endpoint, item);
     }
 
+    public async Task UpdateAsync(object key, T item, CancellationToken cancellationToken = default)
+    {
+        var endpoint = ApiEndpoint(nameof(UpdateAsync), key);
+        logger?.LogInformation("Updating '{entity}' on '{endpoint}'", nameof(T), endpoint);
+        var response = await http.PutAsJsonAsync(endpoint, item, cancellationToken);
+        await response.AssertSuccess();
+        logger?.LogDebug("Updated '{entity}' on '{endpoint}' with content: {content}", nameof(T), endpoint, item);
+    }
+
+    [Obsolete("Inject arguments into HtttpClient derived type")]
     public async Task DeleteAsync(object key, params object[] args)
     {
         var endpoint = ApiEndpoint(nameof(DeleteAsync), key, args);
@@ -84,16 +137,25 @@ public class CrudService<T> {
         logger?.LogDebug("Deleted '{entity}' at '{endpoint}'", nameof(T), endpoint);
     }
 
+    public async Task DeleteAsync(object key, CancellationToken cancellationToken = default)
+    {
+        var endpoint = ApiEndpoint(nameof(DeleteAsync), key);
+        logger?.LogInformation("Deleting '{entity}' at '{endpoint}'", nameof(T), endpoint);
+        var response = await http.DeleteAsync(endpoint, cancellationToken);
+        await response.AssertSuccess();
+        logger?.LogDebug("Deleted '{entity}' at '{endpoint}'", nameof(T), endpoint);
+    }
+
     private string ApiEndpoint(string method, object key, params object[] args)
     {
         try {
-            var baseUrl = string.Format(ApiTemplate, args);
+            var baseUrl = string.Format(Options.CrudEndpoint, args);
             var url = $"{baseUrl}/{key}".TrimEnd('/');
             return url;
         }
         catch(FormatException ex) {
             var argsFormatted = string.Join(',', args?.Select(e => e?.ToString()) ?? Array.Empty<string>());
-            logger?.LogWarning("Formatting problem while constructing endpoint for `CrudService.{method}`.  Typically the endpoint provided has additional placeholders that have not been provided. The endpoint template ({ApiTemplate}), could not be satisifed with arguments ({argsFormatted}).  Inner Exception was:  {ex.Message}", method, ApiTemplate, argsFormatted, ex.Message);
+            logger?.LogWarning("Formatting problem while constructing endpoint for `CrudService.{method}`.  Typically the endpoint provided has additional placeholders that have not been provided. The endpoint template ({ApiTemplate}), could not be satisfied with arguments ({argsFormatted}).  Inner Exception was:  {ex.Message}", method, ApiTemplate, argsFormatted, ex.Message);
             throw new DryException("Error occurred connecting to server", "This is a mis-configuration and not a user error, please see the console output for more information.");
         }
     }
