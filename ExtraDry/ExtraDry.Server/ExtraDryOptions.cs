666ebf136cb187b80b156c5cb97c72096e7b1a1a
Wayne <wayne.scott@outlook.com>
2023-12-22 11:42:56 +1100

    Return configurable problem details for unauthorised response.

--- ExtraDry/ExtraDry.Server/ExtraDryOptions.cs
+++ ExtraDry/ExtraDry.Server/ExtraDryOptions.cs
@@ -5,4 +5,8 @@ public class ExtraDryOptions {
     public string? ForbiddenTitle { get; set; }
 
     public string? ForbiddenMessage { get; set; }
+
+    public string? UnauthorizedTitle { get; set; }
+
+    public string? UnauthorizedMessage { get; set; }
 }
