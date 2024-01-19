c633294e05ecd8e5c58a079021c134fa99664e53
Adrian Akison <adrian@akison.com>
2024-01-19 13:37:58 +1000

    Add Sample.WebApp Blazor client and API server with Docker support

---
+++ ExtraDry/Sample.WebApp/Program.cs
@@ -0,0 +1,39 @@
+using Sample.WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+if(app.Environment.IsDevelopment()) {
+    app.UseWebAssemblyDebugging();
+}
+else {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Sample.WebApp.Client._Imports).Assembly);
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
