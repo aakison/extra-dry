22ac298b77a6404670de164f327046822df320fe
Jaspal Muker <jaspal.muker@fmiworks.com>
2024-02-22 12:35:19 +1000

    Simplified DryStaticFilter

--- ExtraDry/ExtraDry.Blazor/Components/Dry/DryStaticFilter.razor.cs
+++ ExtraDry/ExtraDry.Blazor/Components/Dry/DryStaticFilter.razor.cs
@@ -1,11 +1,6 @@
 namespace ExtraDry.Blazor;
-public partial class DryStaticFilter<TItem> : ComponentBase, IExtraDryComponent
+public partial class DryStaticFilter : ComponentBase, IExtraDryComponent
 {
-    public DryStaticFilter()
-    {
-        ViewModelDescription = new ViewModelDescription(typeof(TItem), this);
-    }
-
     /// <inheritdoc cref="IExtraDryComponent.CssClass "/>
     [Parameter]
     public string CssClass { get; set; } = string.Empty;
@@ -17,20 +12,9 @@ public partial class DryStaticFilter<TItem> : ComponentBase, IExtraDryComponent
     [Parameter]
     public int? InitialHierarchyLevel { get; set; }
 
-    [Parameter]
-    public string? TextFilter { get; set; }
-
-    [Parameter]
-    public Dictionary<string,object>? PropertyFilter { get; set; }
-
-
     /// <inheritdoc cref="DryPageQueryView.PageQueryBuilder" />
     [CascadingParameter]
     public QueryBuilder? QueryBuilder { get; set; }
-
-    private ViewModelDescription ViewModelDescription { get; set; }
-
-    private string CssClasses => DataConverter.JoinNonEmpty(" ", "dry-static-filter", CssClass);
 
     protected override Task OnInitializedAsync()
     {
