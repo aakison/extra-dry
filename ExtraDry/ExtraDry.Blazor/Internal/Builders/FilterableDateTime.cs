bbf20fb4758ae90423120e195d32adf09018ef2e
Wayne <wayne.scott@outlook.com>
2024-02-27 12:01:21 +1100

    Add a filter builder for dates.

---
+++ ExtraDry/ExtraDry.Blazor/Internal/Builders/FilterableDateTime.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ExtraDry.Blazor.Internal;
+
+/// <summary>
+/// A DateTime structure used by the DateTimeFilterBuilder that supports partial date and times
+/// required for filtering.
+/// </summary>
+/// <remarks>
+/// The time portion of the builder has not been implemented at this point, but will be added when
+/// required.
+/// </remarks>
+public struct FilterableDateTime
+{
+    public FilterableDateTime(int year, int? month = null)
+    {
+        ValidateDateTime(year, month ?? 1, 1);
+        Year = year;
+        Month = month;
+    }
+
+    public FilterableDateTime(int year, int month, int day)
+    {
+        ValidateDateTime(year, month, day);
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public FilterableDateTime(DateTime dateTime)
+    {
+        Year = dateTime.Year;
+        Month = dateTime.Month;
+        Day = dateTime.Day;
+    }
+
+    public int Year { get; }
+    public int? Month { get; }
+    public int? Day { get; }
+
+    public bool IsFullDate => Month.HasValue && Day.HasValue;
+
+    public string Build() => $"{Year}-{Month}-{Day}".TrimEnd('-');
+
+    /// <summary>
+    /// Converts the specified string representation of a date and time filter to its 
+    /// FilterableDateTime equivalent.
+    /// </summary>
+    /// <returns>Returns a value that indicates whether the conversion succeeded.</returns>
+    public static FilterableDateTime? TryParseFilterableDateTime(string dateTime)
+    {
+        if(string.IsNullOrEmpty(dateTime)) {
+            return null;
+        }
+
+        var dateTimeParts = dateTime.Split('-');
+        if(dateTimeParts.Length < 1 || dateTimeParts.Length > 3) {
+            return null;
+        }
+        if(dateTimeParts.Any(part => string.IsNullOrEmpty(part) || !Int32.TryParse(part, out _))) {
+            return null;
+        }
+        return dateTimeParts.Length switch {
+            1 => new FilterableDateTime(Convert.ToInt32(dateTimeParts[0], CultureInfo.InvariantCulture)),
+            2 => new FilterableDateTime(Convert.ToInt32(dateTimeParts[0], CultureInfo.InvariantCulture),
+                                        Convert.ToInt32(dateTimeParts[1], CultureInfo.InvariantCulture)),
+            3 => new FilterableDateTime(Convert.ToInt32(dateTimeParts[0], CultureInfo.InvariantCulture),
+                                        Convert.ToInt32(dateTimeParts[1], CultureInfo.InvariantCulture),
+                                        Convert.ToInt32(dateTimeParts[2], CultureInfo.InvariantCulture)),
+            _ => null
+        };
+    }
+
+    private static void ValidateDateTime(int year, int month, int day)
+    {
+        _ = new DateTime(year, month, day);
+    }
+}
--- ExtraDry/ExtraDry.Blazor/Internal/Builders/QueryBuilder.cs
+++ ExtraDry/ExtraDry.Blazor/Internal/Builders/QueryBuilder.cs
@@ -5,10 +5,12 @@
 /// notifications so that display components can sync to the changing filters.  This is the primary
 /// mechanism by which filter components inform table views of filters and required refreshes.
 /// </summary>
-public class QueryBuilder {
+public class QueryBuilder
+{
 
     /// <inheritdoc cref="QueryBuilder" />
-    public QueryBuilder() { 
+    public QueryBuilder()
+    {
         TextFilter = new TextFilterBuilder() { FilterName = "Keywords" };
         Filters.Add(TextFilter);
     }
@@ -87,4 +89,46 @@ public class QueryBuilder {
 
     public int? Skip { get; set; }
 
+    public void ParseFilters(string[] filters)
+    {
+        var notifyChanged = false;
+        foreach(var filter in filters) {
+
+            var filterKeyValue = filter.Split(':');
+            if(filterKeyValue.Length != 2) { continue; }
+
+            var propertyName = filterKeyValue[0];
+            var filterValue = filterKeyValue[1];
+            if(string.IsNullOrEmpty(propertyName) || string.IsNullOrEmpty(filterValue)) { continue; }
+
+            var queryFilter = Filters.FirstOrDefault(f => f.FilterName == propertyName);
+            switch(queryFilter) {
+                case EnumFilterBuilder enumFilter:
+                    if (UpdateEnumFilter(enumFilter, filterValue)) {
+                        notifyChanged = true;
+                    }
+                    break;
+                case DateTimeFilterBuilder dateTimeFilterBuilder:
+                    if(dateTimeFilterBuilder.TryParseFilter(filter)) {
+                        notifyChanged = true;
+                    };
+                    break;
+            }
+        }
+        if(notifyChanged) {
+            NotifyChanged();
+        }
+    }
+
+    private static bool UpdateEnumFilter(EnumFilterBuilder enumFilter, string queryValue)
+    {
+        if(string.IsNullOrEmpty(queryValue)) { return false; }
+
+        var values = queryValue.Split('|');
+        foreach(var value in values) {
+            enumFilter.Values.Add(value);
+        }
+
+        return true;
+    }
 }
