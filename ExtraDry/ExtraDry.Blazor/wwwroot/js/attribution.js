11885648d1c3e77cee980bea169cf67e7ed02cf3
Adrian Akison <adrian@akison.com>
2024-09-11 19:42:41 +1000

    Change location of repos

--- ExtraDry/ExtraDry.Blazor/wwwroot/js/attribution.js
+++ ExtraDry/ExtraDry.Blazor/wwwroot/js/attribution.js
@@ -1,2 +1,2 @@
-console.log(`Blazor Extra Dry by @aakison - https://github.com/fmi-works/extra-dry License - https://github.com/fmi-works/extra-dry/blob/main/LICENSE (MIT License)`);
+console.log(`Blazor Extra Dry by @aakison - https://github.com/akison/extra-dry License - https://github.com/akison/extra-dry/blob/main/LICENSE (MIT License)`);
 
