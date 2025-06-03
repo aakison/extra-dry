4bf64471a0aae2a6b59bda918a3eda0a8d09830e
Adrian Akison <adrian@akison.com>
2025-06-03 17:39:25 +1000

    Add DryInputComboBox on way to DryInput native support

---
+++ ExtraDry/ExtraDry.Blazor/Components/Dry/Forms/DryInputComboBox.razor.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.Metadata;
+
+namespace ExtraDry.Blazor.Forms;
+
+/// <summary>
+/// A DRY wrapper around a combo box input field. Prefer the use of <see cref="DryInput{T}" /> instead
+/// of this component as it is more flexible and supports more data types.
+/// </summary>
+public partial class DryInputComboBox<T>
+    : DryInputBase<T>
+    where T : class
+{
+    [Parameter]
+    public bool ReadOnly { get; set; }
+
+    [Parameter]
+    public IEnumerable<string>? Items { get; set; }
+
+    [Parameter]
+    public IListService<string>? ItemsSource { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        if(Model == null || Property == null) {
+            return;
+        }
+        Value = Property.DisplayValue(Model);
+    }
+
+    private string ReadOnlyCss => ReadOnly ? "readonly" : string.Empty;
+
+    private string CssClasses => DataConverter.JoinNonEmpty(" ", "input", "text", ReadOnlyCss, CssClass);
+
+    private string Value { get; set; } = "";
+
+    private async Task InvokeOnChange(ChangeEventArgs args)
+    {
+        await OnChange.InvokeAsync(args);
+    }
+
+    private async Task HandleChange(ChangeEventArgs args)
+    {
+        if(Property == null || Model == null) {
+            return;
+        }
+        var value = args.Value;
+        Property.SetValue(Model, value);
+        var valid = ValidateProperty();
+
+        await InvokeOnChangeAsync(value);
+        await InvokeOnValidationAsync(valid);
+    }
+}
